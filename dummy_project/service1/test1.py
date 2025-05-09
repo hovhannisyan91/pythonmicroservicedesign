be9c6b99b41628e31c7783113b06e19ddd95dfa7
hovhannisyan91 <khovhannisyan91@yahoo.com>
2025-05-09 13:01:51 +0400

    dummy project shared logs

---
+++ dummy_project/service1/test1.py
@@ -0,0 +1,4 @@
+from logger import logger
+
+logger.info("This is an info message from test1.")
+logger.error("This is an error message from test1.")
---
+++ dummy_project/service2/test2.py
@@ -0,0 +1,4 @@
+from logger import logger
+
+logger.info("This is an info message from test2.")
+logger.error("This is an error message from test2.")
