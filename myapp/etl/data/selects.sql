e3f6538a8a26476ea8820afb4382a539f762cd60
hovhannisyan91 <khovhannisyan91@yahoo.com>
2025-11-11 12:33:16 +0400

    helper

---
+++ myapp/etl/data/selects.sql
@@ -0,0 +1,16 @@
+SELECT * FROM public.customers
+LIMIT 100
+
+SELECT * FROM public.employees
+LIMIT 100
+
+
+SELECT * FROM public.orders
+LIMIT 100
+
+SELECT * FROM public.products
+LIMIT 100
+
+
+SELECT * FROM public.sales
+LIMIT 100
